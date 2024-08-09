efac28cece1feac641a09510bb68969343f99d4e
Young Zhang <young.quinn.zhang@gmail.com>
2024-08-08 20:48:14 -0400

    reformat with 3 spaces

--- client/src/api_calls.rs
+++ client/src/api_calls.rs
@@ -8,17 +8,17 @@ use ethers::providers::{Http, Provider};
 use util::prelude::AsyncResult;
 
 pub(crate) async fn get_block_timestamp(block_number: u64, provider: Arc<Provider<Http>>) -> u64 {
-    let block = provider.get_block(block_number).await.unwrap();
-    u64::try_from(block.unwrap().timestamp).unwrap()
+   let block = provider.get_block(block_number).await.unwrap();
+   u64::try_from(block.unwrap().timestamp).unwrap()
 }
 
 // see: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/IERC20Metadata.sol
 abigen!(IERC20, r#"[function decimals() external view returns (uint8)]"#);
 
 pub async fn get_token_decimals(token_address: Address, provider: Arc<Provider<Http>>) -> AsyncResult<u8> {
-    let token = IERC20::new(token_address, provider);
-    let token_decimal = token.decimals().call().await.unwrap();
-    Ok(token_decimal)
+   let token = IERC20::new(token_address, provider);
+   let token_decimal = token.decimals().call().await.unwrap();
+   Ok(token_decimal)
 }
 
 // This definition comes from the repo: https://github.com/Uniswap/v3-core
@@ -26,161 +26,162 @@ pub async fn get_token_decimals(token_address: Address, provider: Arc<Provider<H
 // towards the bottom of the file, right above "bytecode"
 abigen!(UniswapV3Pool,
         r#"[{
-            "constant": true,
-            "inputs": [],
-            "name": "token0",
-            "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-            "stateMutability": "view",
-            "type": "function"
+           "constant": true,
+           "inputs": [],
+           "name": "token0",
+           "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+           "stateMutability": "view",
+           "type": "function"
         },
         {
-            "constant": true,
-            "inputs": [],
-            "name": "token1",
-            "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-            "stateMutability": "view",
-            "type": "function"
+           "constant": true,
+           "inputs": [],
+           "name": "token1",
+           "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+           "stateMutability": "view",
+           "type": "function"
         }]
        "#,);
 
 pub async fn get_pool_tokens(pool_address: Address, provider: Arc<Provider<Http>>) -> AsyncResult<(Address, Address)> {
-    let pool = UniswapV3Pool::new(pool_address, provider);
-    let token0: Address = pool.token_0().call().await.unwrap();
-    let token1: Address = pool.token_1().call().await.unwrap();
-    Ok((token0, token1))
+   let pool = UniswapV3Pool::new(pool_address, provider);
+   let token0: Address = pool.token_0().call().await.unwrap();
+   let token1: Address = pool.token_1().call().await.unwrap();
+   Ok((token0, token1))
 }
 
 pub fn get_exchange_price(sqrt_price_x96: U160, token0_decimals: u8, token1_decimals: u8) -> f64 {
-    let sqrt_price_x96 = u160_to_f64_lossy(sqrt_price_x96);
-    let price = (sqrt_price_x96 * sqrt_price_x96) / (2.0_f64.powi(192));
+   let sqrt_price_x96 = u160_to_f64_lossy(sqrt_price_x96);
+   let price = (sqrt_price_x96 * sqrt_price_x96) / (2.0_f64.powi(192));
 
-    let token0_decimals = i32::from(token0_decimals);
-    let token1_decimals = i32::from(token1_decimals);
-    let exponent = token1_decimals - token0_decimals;
-    10.0_f64.powi(exponent) / price
+   let token0_decimals = i32::from(token0_decimals);
+   let token1_decimals = i32::from(token1_decimals);
+   let exponent = token1_decimals - token0_decimals;
+   10.0_f64.powi(exponent) / price
 }
 
 fn u160_to_f64_lossy(value: U160) -> f64 {
-    let value_bytes = value.to_be_bytes_trimmed_vec();
-    let u256_value = ethereum_types::U256::from_big_endian(&value_bytes);
-    u256_to_f64_lossy(u256_value)
+   let value_bytes = value.to_be_bytes_trimmed_vec();
+   let u256_value = ethereum_types::U256::from_big_endian(&value_bytes);
+   u256_to_f64_lossy(u256_value)
 }
 
 fn u256_to_f64_lossy(u256_value: ethereum_types::U256) -> f64 {
-    // Reference: https://blog.m-ou.se/floats/
-    // Step 1: Get leading zeroes
-    let leading_zeroes = u256_value.leading_zeros();
-    // Step 2: Get msb to be the farthest left bit
-    let left_aligned = u256_value << leading_zeroes;
-    // Step 3: Shift msb to fit in lower 53 bits of the first u64 (64-53=11)
-    let quarter_aligned = left_aligned >> 11;
-    let mantissa = quarter_aligned.0[3];
-    // Step 4: For the dropped bits - all bits beyond the 53 most significant
-    // We want to know only 2 things. If the msb of the dropped bits is 1 or 0,
-    // and if any of the other bits are 1. (See blog for explanation)
-    // So we take care to preserve the msb bit, while jumbling the rest of the bits
-    // together so that any 1s will survive. If all 0s, then the result will also be 0.
-    let dropped_bits = quarter_aligned.0[1] | quarter_aligned.0[0] | (left_aligned.0[0] & 0xFFFF_FFFF);
-    let dropped_bits = (dropped_bits & 0x7FFF_FFFF_FFFF_FFFF) | (dropped_bits >> 63);
-    let dropped_bits = quarter_aligned.0[2] | dropped_bits;
-    // Step 5: dropped_bits contains the msb of the original bits and an OR-mixed 63 bits.
-    // If msb of dropped bits is 0, it is mantissa + 0
-    // If msb of dropped bits is 1, it is mantissa + 0 only if mantissa the lowest bit is 0
-    // and other bits of the dropped bits are all 0 (which both can be tested with the below all at once)
-    let mantissa = mantissa + ((dropped_bits - (dropped_bits >> 63 & !mantissa)) >> 63);
-    // Step 6: Calculate the exponent
-    // If self is 0, exponent should be 0 (special meaning) and mantissa will end up 0 too
-    // Otherwise, (255 - n) + 1022, so it simplifies to 1277 - n
-    // 1023 and 1022 are the cutoffs for the exponent having the msb next to the decimal point
-    let exponent = if u256_value.is_zero() {
-        0
-    } else {
-        1277 - leading_zeroes as u64
-    };
-    // Step 7: sign bit is always 0, exponent is shifted into place
-    // Use addition instead of bitwise OR to saturate the exponent if mantissa overflows
-    f64::from_bits((exponent << 52) + mantissa)
+   // Reference: https://blog.m-ou.se/floats/
+   // Step 1: Get leading zeroes
+   let leading_zeroes = u256_value.leading_zeros();
+   // Step 2: Get msb to be the farthest left bit
+   let left_aligned = u256_value << leading_zeroes;
+   // Step 3: Shift msb to fit in lower 53 bits of the first u64 (64-53=11)
+   let quarter_aligned = left_aligned >> 11;
+   let mantissa = quarter_aligned.0[3];
+   // Step 4: For the dropped bits - all bits beyond the 53 most significant
+   // We want to know only 2 things. If the msb of the dropped bits is 1 or 0,
+   // and if any of the other bits are 1. (See blog for explanation)
+   // So we take care to preserve the msb bit, while jumbling the rest of the bits
+   // together so that any 1s will survive. If all 0s, then the result will also be 0.
+   let dropped_bits = quarter_aligned.0[1] | quarter_aligned.0[0] | (left_aligned.0[0] & 0xFFFF_FFFF);
+   let dropped_bits = (dropped_bits & 0x7FFF_FFFF_FFFF_FFFF) | (dropped_bits >> 63);
+   let dropped_bits = quarter_aligned.0[2] | dropped_bits;
+   // Step 5: dropped_bits contains the msb of the original bits and an OR-mixed 63 bits.
+   // If msb of dropped bits is 0, it is mantissa + 0
+   // If msb of dropped bits is 1, it is mantissa + 0 only if mantissa the lowest bit is 0
+   // and other bits of the dropped bits are all 0 (which both can be tested with the below all at once)
+   let mantissa = mantissa + ((dropped_bits - (dropped_bits >> 63 & !mantissa)) >> 63);
+   // Step 6: Calculate the exponent
+   // If self is 0, exponent should be 0 (special meaning) and mantissa will end up 0 too
+   // Otherwise, (255 - n) + 1022, so it simplifies to 1277 - n
+   // 1023 and 1022 are the cutoffs for the exponent having the msb next to the decimal point
+   let exponent = if u256_value.is_zero() {
+      0
+   }
+   else {
+      1277 - leading_zeroes as u64
+   };
+   // Step 7: sign bit is always 0, exponent is shifted into place
+   // Use addition instead of bitwise OR to saturate the exponent if mantissa overflows
+   f64::from_bits((exponent << 52) + mantissa)
 }
 
 #[cfg(test)]
 mod tests {
-    use std::convert::TryFrom;
-    use std::env;
-    use std::sync::Arc;
+   use std::convert::TryFrom;
+   use std::env;
+   use std::sync::Arc;
 
-    use chrono::{DateTime, Utc};
-    use ethers::providers::{Http, Provider};
-    use log::info;
-    use util::async_result::AsyncResult;
+   use chrono::{DateTime, Utc};
+   use ethers::providers::{Http, Provider};
+   use log::info;
+   use util::async_result::AsyncResult;
 
-    use crate::api_calls::*;
-    use crate::subscription::tests::get_eth_subscription;
+   use crate::api_calls::*;
+   use crate::subscription::tests::get_eth_subscription;
 
-    fn get_eth_provider() -> Arc<Provider<Http>> {
-        let alchemy_api_key = env::var("ALCHEMY_API_KEY").unwrap();
-        let alchemy_url = format!("https://eth-mainnet.alchemyapi.io/v2/{}", alchemy_api_key);
-        let provider = Provider::<Http>::try_from(alchemy_url).unwrap();
-        Arc::new(provider)
-    }
+   fn get_eth_provider() -> Arc<Provider<Http>> {
+      let alchemy_api_key = env::var("ALCHEMY_API_KEY").unwrap();
+      let alchemy_url = format!("https://eth-mainnet.alchemyapi.io/v2/{}", alchemy_api_key);
+      let provider = Provider::<Http>::try_from(alchemy_url).unwrap();
+      Arc::new(provider)
+   }
 
-    async fn test_get_block_timestamp_async() -> AsyncResult<()> {
-        let subscription = get_eth_subscription();
-        let result = &subscription.params.as_ref().unwrap().result;
-        let block_number = result.block_number();
+   async fn test_get_block_timestamp_async() -> AsyncResult<()> {
+      let subscription = get_eth_subscription();
+      let result = &subscription.params.as_ref().unwrap().result;
+      let block_number = result.block_number();
 
-        let provider = get_eth_provider();
-        let timestamp = get_block_timestamp(block_number, provider).await;
-        let datetime = DateTime::<Utc>::from_timestamp(timestamp as i64, 0).expect("Invalid timestamp");
-        info!("Block {} timestamp: {}", block_number, datetime.format("%Y-%m-%d %H:%M:%S UTC"));
-        Ok(())
-    }
+      let provider = get_eth_provider();
+      let timestamp = get_block_timestamp(block_number, provider).await;
+      let datetime = DateTime::<Utc>::from_timestamp(timestamp as i64, 0).expect("Invalid timestamp");
+      info!("Block {} timestamp: {}", block_number, datetime.format("%Y-%m-%d %H:%M:%S UTC"));
+      Ok(())
+   }
 
-    #[test]
-    fn test_get_block_timestamp() {
-        util_test::logging::init_logging();
-        let _ = tokio_test::block_on(test_get_block_timestamp_async());
-    }
+   #[test]
+   fn test_get_block_timestamp() {
+      util_test::logging::init_logging();
+      let _ = tokio_test::block_on(test_get_block_timestamp_async());
+   }
 
-    async fn test_get_token_decimals_async() -> AsyncResult<()> {
-        let provider = get_eth_provider();
+   async fn test_get_token_decimals_async() -> AsyncResult<()> {
+      let provider = get_eth_provider();
 
-        let usdc_address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-        let usdc_decimal = get_token_decimals(usdc_address.parse().unwrap(), provider.clone()).await.unwrap();
-        assert_eq!(usdc_decimal, 6u8);
+      let usdc_address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+      let usdc_decimal = get_token_decimals(usdc_address.parse().unwrap(), provider.clone()).await.unwrap();
+      assert_eq!(usdc_decimal, 6u8);
 
-        let weth_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
-        let weth_decimal = get_token_decimals(weth_address.parse().unwrap(), provider.clone()).await.unwrap();
-        assert_eq!(weth_decimal, 18u8);
-        Ok(())
-    }
+      let weth_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+      let weth_decimal = get_token_decimals(weth_address.parse().unwrap(), provider.clone()).await.unwrap();
+      assert_eq!(weth_decimal, 18u8);
+      Ok(())
+   }
 
-    #[test]
-    fn test_get_token_decimals() {
-        util_test::logging::init_logging();
-        let _ = tokio_test::block_on(test_get_token_decimals_async());
-    }
+   #[test]
+   fn test_get_token_decimals() {
+      util_test::logging::init_logging();
+      let _ = tokio_test::block_on(test_get_token_decimals_async());
+   }
 
-    async fn test_get_pool_tokens_async() -> AsyncResult<()> {
-        let provider = get_eth_provider();
-        let pool_address = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"; // USDC/wETH pool
-        let (token0, token1) = get_pool_tokens(pool_address.parse().unwrap(), provider).await.unwrap();
-        assert_eq!(token0, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48".parse().unwrap()); // USDC : token0
-        assert_eq!(token1, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".parse().unwrap()); // wETH : token1
-        Ok(())
-    }
+   async fn test_get_pool_tokens_async() -> AsyncResult<()> {
+      let provider = get_eth_provider();
+      let pool_address = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"; // USDC/wETH pool
+      let (token0, token1) = get_pool_tokens(pool_address.parse().unwrap(), provider).await.unwrap();
+      assert_eq!(token0, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48".parse().unwrap()); // USDC : token0
+      assert_eq!(token1, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".parse().unwrap()); // wETH : token1
+      Ok(())
+   }
 
-    #[test]
-    fn test_get_pool_tokens() {
-        util_test::logging::init_logging();
-        let _ = tokio_test::block_on(test_get_pool_tokens_async());
-    }
+   #[test]
+   fn test_get_pool_tokens() {
+      util_test::logging::init_logging();
+      let _ = tokio_test::block_on(test_get_pool_tokens_async());
+   }
 
-    #[test]
-    fn test_get_exchange_price() {
-        util_test::logging::init_logging();
-        // see: https://blog.uniswap.org/uniswap-v3-math-primer
-        let sqrt_price_x96 = U160::from(2018382873588440326581633304624437u128);
-        let price = get_exchange_price(sqrt_price_x96, 6, 18);
-        assert!((price - 1540.82f64).abs() < 0.01);
-    }
+   #[test]
+   fn test_get_exchange_price() {
+      util_test::logging::init_logging();
+      // see: https://blog.uniswap.org/uniswap-v3-math-primer
+      let sqrt_price_x96 = U160::from(2018382873588440326581633304624437u128);
+      let price = get_exchange_price(sqrt_price_x96, 6, 18);
+      assert!((price - 1540.82f64).abs() < 0.01);
+   }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -24,112 +24,108 @@ async fn print_swap_details(subscription: &EthereumSubscription,
                             token1_decimals: u8,
                             provider: Arc<Provider<Http>>)
                             -> AsyncResult<()> {
-    if let Some(params) = &subscription.params {
-        let block_number = &params.result.block_number();
+   if let Some(params) = &subscription.params {
+      let block_number = &params.result.block_number();
 
-        // we make a HTTP REST call to get the block timestamp - this is just for demonstration purposes
-        let timestamp = get_block_timestamp(*block_number, provider).await;
-        let timestamp = DateTime::<Utc>::from_timestamp(timestamp as i64, 0).expect("Invalid timestamp");
+      // we make a HTTP REST call to get the block timestamp - this is just for demonstration purposes
+      let timestamp = get_block_timestamp(*block_number, provider).await;
+      let timestamp = DateTime::<Utc>::from_timestamp(timestamp as i64, 0).expect("Invalid timestamp");
 
-        let data = &params.result.data;
-        let swap_event = swap_event::parse_swap_event_from_data(data).unwrap();
-        let exchange_rate = get_exchange_price(swap_event.sqrt_price_x96, token0_decimals, token1_decimals);
+      let data = &params.result.data;
+      let swap_event = swap_event::parse_swap_event_from_data(data).unwrap();
+      let exchange_rate = get_exchange_price(swap_event.sqrt_price_x96, token0_decimals, token1_decimals);
 
-        println!("timestamp: {} / block: {} / price: ~{:.4}",
-                 timestamp.format("%Y-%m-%d %H:%M:%S UTC"),
-                 block_number,
-                 exchange_rate);
-    }
-    Ok(())
+      println!("timestamp: {} / block: {} / price: ~{:.4}",
+               timestamp.format("%Y-%m-%d %H:%M:%S UTC"),
+               block_number,
+               exchange_rate);
+   }
+   Ok(())
 }
 
 pub async fn spawn_wss_client(pool_address: String, swap_signature: String) -> AsyncResult<()> {
-    tokio::spawn(async move {
-        let alchemy_api_key = env::var("ALCHEMY_API_KEY").unwrap();
-        let alchemy_wss_endpoint = format!("wss://eth-mainnet.ws.alchemyapi.io/v2/{}", alchemy_api_key);
-        println!("endpoint: {}", alchemy_wss_endpoint);
+   tokio::spawn(async move {
+      let alchemy_api_key = env::var("ALCHEMY_API_KEY").unwrap();
+      let alchemy_wss_endpoint = format!("wss://eth-mainnet.ws.alchemyapi.io/v2/{}", alchemy_api_key);
+      println!("endpoint: {}", alchemy_wss_endpoint);
 
-        let json_rpc = format!(r#"{{"jsonrpc":"2.0","method":"eth_subscribe","params":["logs",{{"address":"{}","topics":["{}"]}}],"id":1}}"#,
-                               pool_address, swap_signature);
-        println!("json_rpc: {}", json_rpc);
-        println!();
+      let json_rpc = format!(r#"{{"jsonrpc":"2.0","method":"eth_subscribe","params":["logs",{{"address":"{}","topics":["{}"]}}],"id":1}}"#,
+                             pool_address, swap_signature);
+      println!("json_rpc: {}", json_rpc);
+      println!();
 
-        let alchemy_url = format!("https://eth-mainnet.alchemyapi.io/v2/{}", alchemy_api_key);
-        let provider = Arc::new(Provider::<Http>::try_from(alchemy_url).unwrap());
+      let alchemy_url = format!("https://eth-mainnet.alchemyapi.io/v2/{}", alchemy_api_key);
+      let provider = Arc::new(Provider::<Http>::try_from(alchemy_url).unwrap());
 
-        let (token0, token1) = get_pool_tokens(pool_address.parse().unwrap(), provider.clone()).await.unwrap();
-        let token0_decimals = get_token_decimals(token0, provider.clone()).await.unwrap();
-        let token1_decimals = get_token_decimals(token1, provider.clone()).await.unwrap();
+      let (token0, token1) = get_pool_tokens(pool_address.parse().unwrap(), provider.clone()).await.unwrap();
+      let token0_decimals = get_token_decimals(token0, provider.clone()).await.unwrap();
+      let token1_decimals = get_token_decimals(token1, provider.clone()).await.unwrap();
 
-        loop {
-            match tokio_tungstenite::connect_async(&alchemy_wss_endpoint).await {
-                Ok((ws_stream, _)) => {
-                    let (mut tx, mut rx) = ws_stream.split();
+      loop {
+         match tokio_tungstenite::connect_async(&alchemy_wss_endpoint).await {
+            Ok((ws_stream, _)) => {
+               let (mut tx, mut rx) = ws_stream.split();
 
-                    // Send JSON-RPC subscription
-                    if let Err(e) = tx.send(Message::Text(json_rpc.clone())).await {
-                        eprintln!("Failed to send JSON-RPC: {}", e);
-                        continue;
-                    }
+               // Send JSON-RPC subscription
+               if let Err(e) = tx.send(Message::Text(json_rpc.clone())).await {
+                  eprintln!("Failed to send JSON-RPC: {}", e);
+                  continue;
+               }
 
-                    // Read incoming messages
-                    while let Some(message) = rx.next().await {
-                        match message {
-                            Ok(msg) => {
-                                if let Message::Text(text) = msg {
-                                    // println!();
-                                    // println!("Received:");
-                                    // println!("{}", text);
-                                    // println!();
-                                    let subscription = parse_ethereum_subscription(&text).unwrap();
-                                    // println!("{:#?}", subscription);
-                                    // println!();
-                                    print_swap_details(&subscription,
-                                                       token0_decimals,
-                                                       token1_decimals,
-                                                       provider.clone()).await
-                                                                        .unwrap();
-                                }
-                            },
-                            Err(e) => {
-                                eprintln!("Error reading message: {}", e);
-                                break;
-                            },
+               // Read incoming messages
+               while let Some(message) = rx.next().await {
+                  match message {
+                     Ok(msg) => {
+                        if let Message::Text(text) = msg {
+                           // println!();
+                           // println!("Received:");
+                           // println!("{}", text);
+                           // println!();
+                           let subscription = parse_ethereum_subscription(&text).unwrap();
+                           // println!("{:#?}", subscription);
+                           // println!();
+                           print_swap_details(&subscription, token0_decimals, token1_decimals, provider.clone()).await.unwrap();
                         }
-                    }
-                },
-                Err(e) => {
-                    eprintln!("Failed to connect: {}", e);
-                    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-                },
-            }
-            eprintln!("WebSocket disconnected. Attempting to reconnect...");
-        }
-    });
-    Ok(())
+                     },
+                     Err(e) => {
+                        eprintln!("Error reading message: {}", e);
+                        break;
+                     },
+                  }
+               }
+            },
+            Err(e) => {
+               eprintln!("Failed to connect: {}", e);
+               tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+            },
+         }
+         eprintln!("WebSocket disconnected. Attempting to reconnect...");
+      }
+   });
+   Ok(())
 }
 
 #[tokio::main]
 #[allow(unused_must_use)]
 async fn main() -> AsyncResult<()> {
-    let _ = env::var("ALCHEMY_API_KEY").map_err(|e| {
-                                           eprintln!("Couldn't read ALCHEMY_API_KEY: {}", e);
-                                           anyhow::anyhow!("Failed to read ALCHEMY_API_KEY")
-                                       })?;
+   let _ = env::var("ALCHEMY_API_KEY").map_err(|e| {
+                                         eprintln!("Couldn't read ALCHEMY_API_KEY: {}", e);
+                                         anyhow::anyhow!("Failed to read ALCHEMY_API_KEY")
+                                      })?;
 
-    // USDC/wETH pool
-    let pool_address = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
+   // USDC/wETH pool
+   let pool_address = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
 
-    // wBTC/wETH pool
-    // let pool_address = "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD";
+   // wBTC/wETH pool
+   // let pool_address = "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD";
 
-    // signature of Swap(address,address,int256,int256,uint160,uint128,int24)
-    // see: https://www.4byte.directory/event-signatures/?bytes_signature=0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67
-    let swap_signature = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
+   // signature of Swap(address,address,int256,int256,uint160,uint128,int24)
+   // see: https://www.4byte.directory/event-signatures/?bytes_signature=0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67
+   let swap_signature = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
 
-    spawn_wss_client(pool_address.to_string(), swap_signature.to_string()).await.unwrap();
-    signal::ctrl_c().await?;
-    println!("Received ctrl-c signal, shutting down...");
+   spawn_wss_client(pool_address.to_string(), swap_signature.to_string()).await.unwrap();
+   signal::ctrl_c().await?;
+   println!("Received ctrl-c signal, shutting down...");
 
-    Ok(())
+   Ok(())
 }
